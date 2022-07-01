47cc4c55521ef96f3ceb58c973265f0dc45dcba5
Tony <51972581+tony1658@users.noreply.github.com>
2022-07-02 00:46:54 +0200

    refactor(card): pass props directly from parent

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 
-	import Checkbox from './checkbox.svelte';
-	import Textarea from './textarea.svelte';
-	import DeleteButton from './deleteButton.svelte';
-
 	interface Todo {
 		id: number;
 		isDone: boolean;
 		description: string;
 	}
-
 	export let todo: Todo;
-	export let remove: () => void;
-	export let mark: () => void;
-	export let save: () => void;
 </script>
 
 <div class="card {todo.isDone ? 'bg-green-100' : 'bg-yellow-50'} mb-2 shadow-xl" in:fade out:fade>
 	<div class="card-body">
 		<div class="grid grid-cols-6">
 			<div class="col-end2 col-start-1 flex items-center ">
-				<Checkbox bind:checked={todo.isDone} on:change={mark} />
+				<slot name="checkbox" />
 			</div>
 			<div class="col-span-1 col-end-7 ml-1">
-				<DeleteButton on:click={remove} />
+				<slot name="deleteButton" />
 			</div>
 		</div>
-		<Textarea bind:checked={todo.isDone} bind:description={todo.description} on:change={save} />
+		<slot name="textarea" />
 	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,16 @@
 	import { browser } from '$app/env';
 
 	import Card from '../components/card.svelte';
+	import Checkbox from '../components/checkbox.svelte';
 	import Container from '../components/container.svelte';
+	import DeleteButton from '../components/deleteButton.svelte';
+	import Divider from '../components/divider.svelte';
 	import Header from '../components/header.svelte';
 	import Input from '../components/input.svelte';
 	import InputGroup from '../components/inputGroup.svelte';
 	import NavBar from '../components/navbar.svelte';
 	import SaveButton from '../components/saveButton.svelte';
+	import Textarea from '../components/textarea.svelte';
 
 	interface Todo {
 		id: number;
@@ -88,13 +92,35 @@
 		<div>
 			<Header title="Todo" />
 			{#each todoList.filter((t) => !t.isDone) as todo (todo.id)}
-				<Card {todo} {save} remove={() => remove(todo)} mark={() => mark(todo, true)} />
+				<Card {todo}>
+					<Checkbox bind:checked={todo.isDone} on:change={() => mark(todo, true)} slot="checkbox" />
+					<DeleteButton on:click={() => remove(todo)} slot="deleteButton" />
+					<Textarea
+						bind:checked={todo.isDone}
+						bind:description={todo.description}
+						on:change={save}
+						slot="textarea"
+					/>
+				</Card>
 			{/each}
 		</div>
 		<div>
 			<Header title="Done" />
 			{#each todoList.filter((t) => t.isDone) as todo (todo.id)}
-				<Card {todo} {save} remove={() => remove(todo)} mark={() => mark(todo, false)} />
+				<Card {todo}>
+					<Checkbox
+						bind:checked={todo.isDone}
+						on:change={() => mark(todo, false)}
+						slot="checkbox"
+					/>
+					<DeleteButton on:click={() => remove(todo)} slot="deleteButton" />
+					<Textarea
+						bind:checked={todo.isDone}
+						bind:description={todo.description}
+						on:change={save}
+						slot="textarea"
+					/>
+				</Card>
 			{/each}
 		</div>
 	</Divider>
