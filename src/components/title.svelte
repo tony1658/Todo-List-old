83678212e5636c65d2c5c6e9004b05a0ec47222c
Tony <51972581+tony1658@users.noreply.github.com>
2022-07-04 02:14:07 +0200

    refactor(title): simplify classes

--- src/components/title.svelte
+++ src/components/title.svelte
@@ -3,8 +3,4 @@
 	export let isPageTitle: boolean = false;
 </script>
 
-{#if isPageTitle}
-	<h1 class="py-4 text-center text-4xl">{title}</h1>
-{:else}
-	<h1 class="my-1 text-center font-bold underline">{title}</h1>
-{/if}
+<h1 class="py-4 text-center px-40 {isPageTitle ? 'text-4xl' : 'text-2xl'}">{title}</h1>
