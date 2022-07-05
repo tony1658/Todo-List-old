fef3038f1f36dd4e1c70580a4570f38470ef9bff
Tony <51972581+tony1658@users.noreply.github.com>
2022-07-06 00:06:29 +0200

    refactor(inputGroup): center inside component instead
    the header would otherwise move when todos are added.

--- src/components/inputGroup.svelte
+++ src/components/inputGroup.svelte
@@ -1,3 +1,9 @@
-<div class="input-group">
-	<slot />
-</div>
+<script lang="ts">
+	import Container from './container.svelte';
+</script>
+
+<Container>
+	<div class="place input-group">
+		<slot />
+	</div>
+</Container>
