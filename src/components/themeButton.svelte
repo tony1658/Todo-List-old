0862a61bf99c8982d00fde6a90140834453646ff
Tony <51972581+tony1658@users.noreply.github.com>
2022-07-06 00:00:05 +0200

    feat: add themebutton

---
+++ src/components/themeButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import MoonIcon from './icons/MoonIcon.svelte';
+	import SunIcon from './icons/SunIcon.svelte';
+
+	export let theme = 'light';
+</script>
+
+<button class="btn btn-outline btn-square btn-sm">
+	{#if theme === 'light'}
+		<MoonIcon />
+	{:else}
+		<SunIcon />
+	{/if}
+</button>
