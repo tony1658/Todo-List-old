eb0370aa287cdf65e02f7d15b529ae70c5d2c9aa
Tony Zhou <9445891-tony_zh@users.noreply.gitlab.com>
2022-07-14 00:22:28 +0200

    refactor(Card): add top padding

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -13,7 +13,7 @@
 	const remove: (todo: Todo) => void = getContext('remove');
 </script>
 
-<div class="card {todo.isDone ? 'bg-green-100' : 'bg-yellow-50'} mb-2 shadow-xl" in:fade out:fade>
+<div class="card {todo.isDone ? 'bg-green-100' : 'bg-yellow-50'} my-2 shadow-xl" in:fade out:fade>
 	<div class="card-body">
 		<div class="grid grid-cols-6">
 			<div class="col-end2 col-start-1 flex items-center ">
