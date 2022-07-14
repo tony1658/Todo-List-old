6b3aff0d030de407bc8b811c8f001e1d71e9b5ee
Tony Zhou <9445891-tony_zh@users.noreply.gitlab.com>
2022-07-14 23:34:04 +0200

    feat(EditButton)

---
+++ src/components/EditButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import EditIcon from '$assets/EditIcon.svelte';
+</script>
+
+<button
+	aria-label="Edit todo button"
+	class="btn btn-ghost btn-square btn-xs text-base-200"
+	on:click
+>
+	<EditIcon />
+</button>
